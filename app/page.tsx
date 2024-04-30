b19e6fef131762e9a7f1a2f99f688f80d88bd719
George Vieira <georgevieiranrb@gmail.com>
2024-04-30 14:23:05 -0300

    feat: add promo-banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "./_components/header";
+import Image from "next/image";
 import Search from "./_components/search";
 import CategoryList from "./_components/category-list";
 const name = "";
@@ -7,11 +8,25 @@ const Home = () => {
   return (
     <div className="">
       <Header></Header>
+
       <div className="px-5 pt-6">
         <Search></Search>
       </div>
+
       <div className="px-5 pt-6">
         <CategoryList></CategoryList>
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-full w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        ></Image>
       </div>
     </div>
   );
